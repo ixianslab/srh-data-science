6dfc9a89fd2059881165441b94434e30e2bb65e7
eotp <krois@zedat.fu-berlin.de>
2022-03-06 01:43:38 +0100

    setup basecase

---
+++ src/notebook_env.py
@@ -0,0 +1,34 @@
+import sys
+import socket
+
+# set plotting font style
+from matplotlib import rcParams
+
+rcParams["font.family"] = "sans-serif"
+rcParams["font.sans-serif"] = ["Arial"]
+rcParams["figure.figsize"] = (12, 6)
+rcParams["xtick.labelsize"] = 16
+rcParams["ytick.labelsize"] = 16
+rcParams["legend.fontsize"] = "large"
+
+# set pandas display options
+import pandas as pd
+
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+
+# clean namespace
+del rcParams
+
+# machine/server
+print(
+    f"\n---------------------------------\nWorking on the host: {socket.gethostname()}"
+)
+
+# Python version
+print(f"\n---------------------------------\nPython version: {sys.version}")
+
+# Python kernel
+print(f"\n---------------------------------\nPython interpreter: {sys.executable}")
+
+
